7c4b08a11e282113883eab40017e948e9f81fc78
Tanmay <tanmayckulkarni@gmail.com>
2022-01-03 18:40:12 +0530

    Added tree generation from postorder traversal.

--- Trees/TreeFromTraversals.cpp
+++ Trees/TreeFromTraversals.cpp
@@ -1,4 +1,4 @@
-// BT Creation & displaying using all 3 traversals [RECURSIVE APPROACH]
+// Creation of BT from Pre/Post & Inorder Traversals.
 #include <iostream>
 #include "QnStkCPP.h"
 
@@ -12,7 +12,8 @@ public:
     void Inorder(Node *root);
     void Levelorder(Node *p);
     int SearchInorder(int inArray[], int inStart, int inEnd, int data);
-    Node *GenerateTreeFromTraversals(int *inorder, int *preorder, int inStart, int inEnd);
+    Node *GenerateTreeFromPreorder(int *inorder, int *preorder, int inStart, int inEnd);
+    Node *GenerateTreeFromPostorder(int *inorder, int *postorder, int inStart, int inEnd);
 };
 
 // Preorder function
@@ -107,38 +108,76 @@ void Tree::Levelorder(Node *p)
     }
 }
 
+// Search element of pre/post in inorder array to split list.
 int Tree::SearchInorder(int inArray[], int inStart, int inEnd, int data)
 {
     for (int i = inStart; i <= inEnd; i++)
     {
         if (inArray[i] == data)
         {
+            // returns index of matched element.
             return i;
         }
     }
     return -1;
 }
 
-Node *Tree ::GenerateTreeFromTraversals(int *inorder, int *preorder, int inStart, int inEnd)
+// Generating Tree from preorder traversals.
+Node *Tree ::GenerateTreeFromPreorder(int *inorder, int *preorder, int inStart, int inEnd)
 {
+    // once only initialized!
     static int preIndex = 0;
 
     if (inStart > inEnd)
     {
+        // returns NULL as no child/data now!
         return nullptr;
     }
 
+    // Create node of preoder data moving L to R.
     Node *node = new Node(preorder[preIndex++]);
 
+    // if only 1 element there within bound!
     if (inStart == inEnd)
     {
         return node;
     }
 
+    // Get split index to partition tree.
     int splitIndex = SearchInorder(inorder, inStart, inEnd, node->data);
-    node->lchild = GenerateTreeFromTraversals(inorder, preorder, inStart, splitIndex - 1);
-    node->rchild = GenerateTreeFromTraversals(inorder, preorder, splitIndex + 1, inEnd);
+    node->lchild = GenerateTreeFromPreorder(inorder, preorder, inStart, splitIndex - 1);
+    node->rchild = GenerateTreeFromPreorder(inorder, preorder, splitIndex + 1, inEnd);
 
+    // return the root node at last.
+    return node;
+}
+
+// Generating Tree from postorder traversals.
+Node *Tree ::GenerateTreeFromPostorder(int *inorder, int *postorder, int inStart, int inEnd)
+{
+    // once only initialized!
+    static int postIndex = inEnd;
+
+    if (inStart > inEnd)
+    {
+        // returns NULL as no child/data now!
+        return nullptr;
+    }
+    // Create node of postoder data moving R to L.
+    Node *node = new Node(postorder[postIndex--]);
+
+    // if only 1 element there within bound!
+    if (inStart == inEnd)
+    {
+        return node;
+    }
+
+    // Get split index to partition tree.
+    int splitIndex = SearchInorder(inorder, inStart, inEnd, node->data);
+    node->rchild = GenerateTreeFromPostorder(inorder, postorder, splitIndex + 1, inEnd);
+    node->lchild = GenerateTreeFromPostorder(inorder, postorder, inStart, splitIndex - 1);
+
+    // return the root node at last.
     return node;
 }
 
@@ -146,20 +185,27 @@ Node *Tree ::GenerateTreeFromTraversals(int *inorder, int *preorder, int inStart
 int main()
 {
     Tree t;
-    int *inorder, *preorder, n;
-    cout << "Enter size of i/p Preorder & Inorder: ";
+    int *inorder, *preorder, *postorder, n;
+    cout << "Enter size of i/p Pre/Post order & Inorder: ";
     cin >> n;
 
     inorder = new int[n];
-    preorder = new int[n];
+    // preorder = new int[n];
+    postorder = new int[n];
 
     // int preorder[] = {4, 7, 9, 6, 3, 2, 5, 8, 1};
     // int inorder[] = {7, 6, 9, 3, 4, 5, 8, 2, 1};
 
-    cout << "Enter Preorder for tree: ";
+    /*cout << "Enter Preorder for tree: ";
     for (int i = 0; i < n; i++)
     {
         cin >> preorder[i];
+    }*/
+
+    cout << "Enter Postorder for tree: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> postorder[i];
     }
 
     cout << "Enter Inorder for tree: ";
@@ -168,7 +214,8 @@ int main()
         cin >> inorder[i];
     }
 
-    Node *T = t.GenerateTreeFromTraversals(inorder, preorder, 0, n - 1);
+    // Node *T = t.GenerateTreeFromPreorder(inorder, preorder, 0, n - 1);
+    Node *T = t.GenerateTreeFromPostorder(inorder, postorder, 0, n - 1);
 
     cout << "\nThe Preorder is: "
          << "\n";
