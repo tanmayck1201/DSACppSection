403b938c9d0af640b2c3f58e683866befd65383e
Tanmay <tanmayckulkarni@gmail.com>
2022-01-07 12:28:28 +0530

    Graph traversals updated!

--- Graphs/GraphTraversals.cpp
+++ Graphs/GraphTraversals.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 
+// BFS traversal
 void BFS(int **G, int start, int n)
 {
     int i = start;
+    // visited array - elements that are printed.
     int *visited;
     visited = new int[n + 1];
     for (int i = 0; i <= n; i++)
@@ -20,11 +22,14 @@ void BFS(int **G, int start, int n)
 
     while (!isEmpty())
     {
+        // dequeue element to explore it furthure!
         i = dequeue();
         for (int j = 1; j <= n; j++)
         {
+            // traverse all colmns of that vertex element.
             if (G[i][j] == 1 && visited[j] == 0)
             {
+                // if edge find & not visited till now do -
                 cout << j << " ";
                 visited[j] = 1;
                 enqueue(j);
@@ -43,6 +48,7 @@ int *visit(int n)
     return visited;
 }
 
+// DFS traversal using recursion
 void DFS(int **G, int start, int n, int visited[])
 {
     if (visited[start] == 0)
@@ -54,7 +60,49 @@ void DFS(int **G, int start, int n, int visited[])
         {
             if (G[start][j] == 1 && visited[j] == 0)
             {
+                // this call will break loop exec for current vertex
+                // & will start exploring futhure as per dfs algo
                 DFS(G, j, n, visited);
+                // while returning call loop will complete & if any
+                // elements are still not done, will visit it!
+            }
+        }
+    }
+}
+
+// dfs using stack
+void dfsstack(int **G, int start, int n)
+{
+    int u = start;
+    int *visited = new int[n];
+    for (int i = 0; i <= n; i++)
+    {
+        visited[i] = 0;
+    }
+    stack<int> stk(15);
+    stk.push(u);
+
+    while (!stk.IsEmpty())
+    {
+        u = stk.stacktop();
+        stk.pop();
+
+        // condition will execute for those vertexes
+        // who have not done till now
+        if (visited[u] != 1)
+        {
+            cout << u << " " << flush;
+            visited[u] = 1;
+
+            for (int v = n; v >= 0; v--)
+            {
+                if (G[u][v] == 1 && visited[v] == 0)
+                {
+                    // will push every ele on stack
+                    // be it there before existing in stack
+                    // but while exec only non-visited vertex will exec loop & all!
+                    stk.push(v);
+                }
             }
         }
     }
@@ -62,7 +110,7 @@ void DFS(int **G, int start, int n, int visited[])
 
 int main()
 {
-    int **A, n, edges;
+    int **A, n, edges, p;
     cout << "Enter the no. of vertices: ";
     cin >> n;
 
@@ -92,11 +140,16 @@ int main()
         A[x][y] = 1;
     }
 
-    BFS(A, 4, n);
+    cout << "Enter starting vertex: ";
+    cin >> p;
+
+    BFS(A, p, n);
     int *visited = visit(n);
     cout << endl;
     cout << "DFS: ";
-    DFS(A, 4, n, visited);
+    DFS(A, p, n, visited);
+    cout << "\nDFS by Stack: ";
+    dfsstack(A, p, n);
 
     return 0;
 }
